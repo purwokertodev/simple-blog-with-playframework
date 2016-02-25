092a4d00167a01fd8b1eb1f8c737696a137dac18
Wuriyanto <wuriyanto48@yahoo.co.id>
2016-02-25 10:48:51 +0700

    update

--- target/scala-2.10/src_managed/main/routes_routing.scala
+++ target/scala-2.10/src_managed/main/routes_routing.scala
@@ -1,6 +1,6 @@
 // @SOURCE:E:/JIMAT/play-project/user_management/conf/routes
-// @HASH:941ab40526a4503d31db99447ee449f8dded63df
-// @DATE:Thu Feb 25 10:47:32 ICT 2016
+// @HASH:1a2d951b729702003e691ad39b27ff6520517c63
+// @DATE:Thu Feb 25 10:48:18 ICT 2016
 
 
 import play.core._
