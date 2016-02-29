c7efd18396facdc9a85de4069f435ea8e4e28c49
Wuriyanto <wuriyanto48@yahoo.co.id>
2016-02-29 20:14:00 +0700

    image preview

--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -42,6 +42,7 @@ Seq[Any](format.raw/*1.32*/("""
         <script src=""""),_display_(Seq[Any](/*14.23*/routes/*14.29*/.Assets.at("javascripts/bootstrap.min.js"))),format.raw/*14.71*/("""" type="text/javascript"></script>
 		<script src=""""),_display_(Seq[Any](/*15.17*/routes/*15.23*/.Assets.at("javascripts/password-check.js"))),format.raw/*15.66*/("""" type="text/javascript"></script>
         <script src=""""),_display_(Seq[Any](/*16.23*/routes/*16.29*/.Assets.at("javascripts/my-play.js"))),format.raw/*16.65*/("""" type="text/javascript"></script>
+		<script src=""""),_display_(Seq[Any](/*17.17*/routes/*17.23*/.Assets.at("javascripts/upload_prev.js"))),format.raw/*17.63*/("""" type="text/javascript"></script>
     </head>
     <body>
         <div class="jumbotron text-center">
@@ -55,13 +56,13 @@ Seq[Any](format.raw/*1.32*/("""
               <a class="navbar-brand" href="#">Play Blog</a>
             </div>
             <ul class="nav navbar-nav">
-              <li><a href=""""),_display_(Seq[Any](/*30.29*/routes/*30.35*/.Application.index())),format.raw/*30.55*/("""">Home</a></li>
-              <li><a href=""""),_display_(Seq[Any](/*31.29*/routes/*31.35*/.Registration.registration())),format.raw/*31.63*/("""">Registration</a></li>
-              <li><a href=""""),_display_(Seq[Any](/*32.29*/routes/*32.35*/.Application.login())),format.raw/*32.55*/("""">Login</a></li>
+              <li><a href=""""),_display_(Seq[Any](/*31.29*/routes/*31.35*/.Application.index())),format.raw/*31.55*/("""">Home</a></li>
+              <li><a href=""""),_display_(Seq[Any](/*32.29*/routes/*32.35*/.Registration.registration())),format.raw/*32.63*/("""">Registration</a></li>
+              <li><a href=""""),_display_(Seq[Any](/*33.29*/routes/*33.35*/.Application.login())),format.raw/*33.55*/("""">Login</a></li>
             </ul>
           </div>
         </nav>
-            """),_display_(Seq[Any](/*36.14*/content)),format.raw/*36.21*/("""
+            """),_display_(Seq[Any](/*37.14*/content)),format.raw/*37.21*/("""
         </div>
     </body>
 </html>
@@ -77,11 +78,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Mon Feb 29 10:59:29 ICT 2016
+                    DATE: Mon Feb 29 17:56:51 ICT 2016
                     SOURCE: E:/JIMAT/play-project/user_management/app/views/main.scala.html
-                    HASH: 9a6ec65b65d0657561720ae4053a02daa15ea464
-                    MATRIX: 778->1|902->31|996->90|1022->95|1120->158|1134->164|1189->198|1281->255|1295->261|1355->300|1448->357|1463->363|1529->406|1627->468|1642->474|1696->506|1758->532|1773->538|1840->583|1934->641|1949->647|2009->685|2103->743|2118->749|2182->791|2270->843|2285->849|2350->892|2444->950|2459->956|2517->992|3058->1497|3073->1503|3115->1523|3196->1568|3211->1574|3261->1602|3350->1655|3365->1661|3407->1681|3527->1765|3556->1772
-                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|39->11|39->11|39->11|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|43->15|43->15|43->15|44->16|44->16|44->16|58->30|58->30|58->30|59->31|59->31|59->31|60->32|60->32|60->32|64->36|64->36
+                    HASH: 13fdbce136539dfdae89648756aac560d52f62b8
+                    MATRIX: 778->1|902->31|996->90|1022->95|1120->158|1134->164|1189->198|1281->255|1295->261|1355->300|1448->357|1463->363|1529->406|1627->468|1642->474|1696->506|1758->532|1773->538|1840->583|1934->641|1949->647|2009->685|2103->743|2118->749|2182->791|2270->843|2285->849|2350->892|2444->950|2459->956|2517->992|2605->1044|2620->1050|2682->1090|3223->1595|3238->1601|3280->1621|3361->1666|3376->1672|3426->1700|3515->1753|3530->1759|3572->1779|3692->1863|3721->1870
+                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|39->11|39->11|39->11|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|43->15|43->15|43->15|44->16|44->16|44->16|45->17|45->17|45->17|59->31|59->31|59->31|60->32|60->32|60->32|61->33|61->33|61->33|65->37|65->37
                     -- GENERATED --
                 */
             
--- target/scala-2.10/src_managed/main/views/html/registration.template.scala
+++ target/scala-2.10/src_managed/main/views/html/registration.template.scala
@@ -57,8 +57,30 @@ Seq[Any](format.raw/*1.25*/("""
                 <div class="col-sm-4">
                     <input type="file" required="true" class="form-control" id="picture" name="picture"/>
                 </div>
-
             </div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="picture">Picture preview:</label>
+				<div class="col-sm-4">
+					<img id="image_upload_preview" class="img-rounded" alt="My Picture" width="304" height="236">
+				</div>
+			</div>
+			<script type="text/javascript">
+				    function readURL(input) """),format.raw/*40.33*/("""{"""),format.raw/*40.34*/("""
+					if (input.files && input.files[0]) """),format.raw/*41.41*/("""{"""),format.raw/*41.42*/("""
+						var reader = new FileReader();
+
+						reader.onload = function (e) """),format.raw/*44.36*/("""{"""),format.raw/*44.37*/("""
+							$('#image_upload_preview').attr('src', e.target.result);
+						"""),format.raw/*46.7*/("""}"""),format.raw/*46.8*/("""
+
+						reader.readAsDataURL(input.files[0]);
+					"""),format.raw/*49.6*/("""}"""),format.raw/*49.7*/("""
+				"""),format.raw/*50.5*/("""}"""),format.raw/*50.6*/("""
+
+				$("#picture").change(function () """),format.raw/*52.38*/("""{"""),format.raw/*52.39*/("""
+					readURL(this);
+				"""),format.raw/*54.5*/("""}"""),format.raw/*54.6*/(""");
+			</script>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="email">Email address:</label>
                 <div class="col-sm-4">
@@ -89,7 +111,7 @@ Seq[Any](format.raw/*1.25*/("""
 			</div>
             <button type="submit" class="btn btn-primary" id="sm_button" disabled="true">Submit</button>
         </div>
-        """)))})),format.raw/*64.10*/("""
+        """)))})),format.raw/*86.10*/("""
     </div>
 </div>
 
@@ -105,11 +127,11 @@ Seq[Any](format.raw/*1.25*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri Feb 26 16:49:42 ICT 2016
+                    DATE: Mon Feb 29 20:08:40 ICT 2016
                     SOURCE: E:/JIMAT/play-project/user_management/app/views/registration.scala.html
-                    HASH: 741e322f0bfc662129eb5b414446c40bb7d4517c
-                    MATRIX: 787->1|921->24|951->46|988->49|1013->66|1051->67|1232->212|1269->240|1309->242|1479->376|1493->381|1529->395|1597->431|1644->442|1744->532|1784->533|4233->2950
-                    LINES: 26->1|30->1|32->4|33->5|33->5|33->5|40->12|40->12|40->12|43->15|43->15|43->15|45->17|46->18|46->18|46->18|92->64
+                    HASH: b25b3e5ed5803ea09e11c9485a8f26d080e99320
+                    MATRIX: 787->1|921->24|951->46|988->49|1013->66|1051->67|1232->212|1269->240|1309->242|1479->376|1493->381|1529->395|1597->431|1644->442|1744->532|1784->533|2868->1589|2897->1590|2967->1632|2996->1633|3101->1710|3130->1711|3230->1784|3258->1785|3339->1839|3367->1840|3400->1846|3428->1847|3497->1888|3526->1889|3580->1916|3608->1917|5347->3624
+                    LINES: 26->1|30->1|32->4|33->5|33->5|33->5|40->12|40->12|40->12|43->15|43->15|43->15|45->17|46->18|46->18|46->18|68->40|68->40|69->41|69->41|72->44|72->44|74->46|74->46|77->49|77->49|78->50|78->50|80->52|80->52|82->54|82->54|114->86
                     -- GENERATED --
                 */
             
